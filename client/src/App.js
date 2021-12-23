c5091266b5e9d6af37304a09a5e7c0ed5ef280f6
Ricardo Costa <rcostaj00@gmail.com>
2021-12-23 16:32:43 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import Entrar from './components/entrar/Entrar';
 import Cadastro from './components/cadastro/Cadastro'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-
-
 function App() {
   return (
    <Router>
@@ -12,9 +10,7 @@ function App() {
        <Route exact path='/' component={Entrar} />
        <Route path='/cadastro' component={Cadastro} />
      </Switch>
-
    </Router>
   );
 }
-
 export default App;
